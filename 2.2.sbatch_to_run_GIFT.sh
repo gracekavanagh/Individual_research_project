545474b3d993cd92f3e56e5d28579329db6f8549
gracekavanagh <151543250+gracekavanagh@users.noreply.github.com>
2024-07-30 17:12:53 +0100

    Update 2.2.sbatch_to_run_GIFT.sh

--- 2.2.sbatch_to_run_GIFT.sh
+++ 2.2.sbatch_to_run_GIFT.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem=250g
 #SBATCH --time=10:00:00
 #SBATCH --job-name=SLiM
-#SBATCH --output=/gpfs01/home/mbygk5/individual_project/GIFT/error.txt
+#SBATCH --output=/gpfs01/home/mbygk5/individual_project/test/error.txt
 #SBATCH --mail-user=mbygk5@exmail.nottingham.ac.uk
 
 # Load Conda
@@ -25,15 +25,15 @@ Rscript -e "installed.packages()"
 
 # Run Python script
 echo "Running Python script"
-python3 /gpfs01/home/mbygk5/individual_project/GIFT/sians_code/GIFT_code.py \
-    -v /gpfs01/home/mbygk5/individual_project/assigning_phenotype/vcf_for_downstream_analysis/simul$
-    -f /gpfs01/home/mbygk5/individual_project/assigning_phenotype/phenotypes.csv \
+python3 /gpfs01/home/mbygk5/individual_project/test/physics_GWAS_OOP.py \
+    -v /gpfs01/home/mbygk5/individual_project/test/vcf_for_downstream_analysis/simulation_output_updated.vcf \
+    -f /gpfs01/home/mbygk5/individual_project/test/phenotypes.csv \
     -p Phenotype \
-    -o /gpfs01/home/mbygk5/individual_project/GIFT/output.csv
+    -o /gpfs01/home/mbygk5/individual_project/test/output.csv
 
 # Run R scripts
 echo "Running R scripts"
-Rscript /gpfs01/home/mbygk5/individual_project/GIFT/output_absolute_theta.R
-Rscript /gpfs01/home/mbygk5/individual_project/GIFT/output_pSNP4.R
-Rscript /gpfs01/home/mbygk5/individual_project/GIFT/output_pSNP5.R
+Rscript /gpfs01/home/mbygk5/individual_project/test/output_absolute_theta.R
+Rscript /gpfs01/home/mbygk5/individual_project/test/output_pSNP4.R
+Rscript /gpfs01/home/mbygk5/individual_project/test/output_pSNP5.R
 
