5cae21d14e19eb2d4982f21cc70083ba3c7eabf4
gracekavanagh <151543250+gracekavanagh@users.noreply.github.com>
2024-07-30 19:17:24 +0100

    Update extract_phenotypes.py

--- extract_phenotypes.py
+++ extract_phenotypes.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
 
-"""
-This script extracts phenotype information from the SLiM simulation output
-and saves it into a CSV file.
-"""
-
 import pandas as pd
 
-# Paths to the SLiM simulation output and the phenotype CSV output
+# paths to SLiM output and new phenotype file
 simulation_output_path = "/gpfs01/home/mbygk5/individual_project/assigning_phenotype/simulation_output.txt"
 phenotypes_output_path = "/gpfs01/home/mbygk5/individual_project/assigning_phenotype/phenotypes.csv"
 
+#extracting the phenotypic data
 def extract_phenotypes(simulation_output_path, phenotypes_output_path):
-    """Extract phenotypes from the SLiM simulation output and save to a CSV file."""
     phenotypes = []
 
     with open(simulation_output_path, 'r') as f:
@@ -30,7 +25,6 @@ def extract_phenotypes(simulation_output_path, phenotypes_output_path):
 
     phenotypes_df = pd.DataFrame(phenotypes, columns=['Individual_ID', 'Phenotype'])
     phenotypes_df.to_csv(phenotypes_output_path, index=False)
-    print(f"Phenotypes saved to {phenotypes_output_path}")
 
 def main():
     extract_phenotypes(simulation_output_path, phenotypes_output_path)
