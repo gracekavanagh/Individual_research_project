9438d0b984a788631ea3203253208f8a86fcb005
gracekavanagh <151543250+gracekavanagh@users.noreply.github.com>
2024-07-30 19:23:23 +0100

    Update sbatch_to_run_GIFT.sh

--- sbatch_to_run_GIFT.sh
+++ sbatch_to_run_GIFT.sh
@@ -9,22 +9,17 @@
 #SBATCH --output=/gpfs01/home/mbygk5/individual_project/test/error.txt
 #SBATCH --mail-user=mbygk5@exmail.nottingham.ac.uk
 
-# Load Conda
+#load environment
 source $HOME/.bash_profile
-
-# Activate Conda environment with R
 conda activate ngs_pipe_env_gatk4
 
-# Verify R installation
-echo "Checking R installation"
+#verifying R installation
 Rscript -e "version"
 
-# Verify R packages
-echo "Checking installed R packages"
+#verifying R packages
 Rscript -e "installed.packages()"
 
-# Run Python script
-echo "Running Python script"
+#running python script
 python3 /gpfs01/home/mbygk5/individual_project/test/physics_GWAS_OOP.py \
     -v /gpfs01/home/mbygk5/individual_project/test/vcf_for_downstream_analysis/simulation_output_updated.vcf \
     -f /gpfs01/home/mbygk5/individual_project/test/phenotypes.csv \
@@ -32,7 +27,6 @@ python3 /gpfs01/home/mbygk5/individual_project/test/physics_GWAS_OOP.py \
     -o /gpfs01/home/mbygk5/individual_project/test/output.csv
 
 # Run R scripts
-echo "Running R scripts"
 Rscript /gpfs01/home/mbygk5/individual_project/test/output_absolute_theta.R
 Rscript /gpfs01/home/mbygk5/individual_project/test/output_pSNP4.R
 Rscript /gpfs01/home/mbygk5/individual_project/test/output_pSNP5.R
