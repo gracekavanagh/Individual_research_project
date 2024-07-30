5b152bc426194c07eb5dcff752b30f8a8d9f2f64
gracekavanagh <151543250+gracekavanagh@users.noreply.github.com>
2024-07-30 15:30:23 +0100

    Update analysis.sh

--- analysis.sh
+++ analysis.sh
@@ -8,7 +8,7 @@
 #SBATCH --mem=50g
 #SBATCH --time=10:00:00
 #SBATCH --job-name=thaliana_SNP_analysis
-#SBATCH --output=/gpfs01/home/mbygk5/individual_project/thaliana_SNP_analysis/thaliana_SNP_analysis.out
+#SBATCH --output=/gpfs01/home/mbygk5/individual_project/test/thaliana_SNP_analysis.out
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=mbygk5@exmail.nottingham.ac.uk
 
@@ -17,7 +17,7 @@
 # Define input and output files
 VCF_FILE='/gpfs01/home/mbygk5/individual_project/test/filled_af.vcf.gz'
 REF_GENOME='/gpfs01/home/mbygk5/individual_project/test/Thaliana_genome.fas'
-OUTPUT_PREFIX='/gpfs01/home/mbygk5/individual_project/thaliana_SNP_analysis/thaliana_snp_analysis'
+OUTPUT_PREFIX='/gpfs01/home/mbygk5/individual_project/test/thaliana_snp_analysis'
 
 # Index the reference genome if not already indexed
 if [ ! -f "${REF_GENOME}.fai" ]; then
