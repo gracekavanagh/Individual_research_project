1c2b0917ab3f6d5a3a8752dbf0ae0ed7a6317b42
gracekavanagh <151543250+gracekavanagh@users.noreply.github.com>
2024-07-30 15:11:49 +0100

    Update and rename 02.vcf_prep_and_AF.sh to prep_and_index.sh

--- prep_and_index.sh
+++ prep_and_index.sh
@@ -1,5 +1,5 @@
 
-#############################################run this to prep vcf and add allele freq###########################
+#############################################run this to prep vcf 
 
 #!/bin/bash
 #SBATCH --partition=defq
@@ -12,6 +12,18 @@
 #SBATCH --output=/gpfs01/home/mbygk5/individual_project/thaliana_SNP_analysis.out
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=mbygk5@exmail.nottingham.ac.uk
+
+echo "Starting job at $(date)"
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate combined_env
+
+# Paths
+REFERENCE="/gpfs01/home/mbygk5/individual_project/test/Thaliana_genome.fas"
+VCF="/gpfs01/home/mbygk5/individual_project/test/thaliana.vcf"
+COMPRESSED_VCF="${VCF}.gz"
+INDEXED_VCF="${COMPRESSED_VCF}.tbi"
+OUTPUT_TABLE="/gpfs01/home/mbygk5/individual_project/test/table.txt"
+
 
 echo "Starting job at $(date)"
 source ~/miniconda3/etc/profile.d/conda.sh
@@ -49,22 +61,4 @@ if [ ! -f "filled_af.vcf.gz" ]; then
     tabix -p vcf filled_af.vcf.gz
 fi
 
-# Step 5: Run GATK VariantsToTable
-echo "Running GATK VariantsToTable"
-gatk VariantsToTable \
-    -V filled_af.vcf.gz \
-    -F CHROM -F POS -F ID -F REF -F ALT -F QUAL -F FILTER -F AF \
-    -O $OUTPUT_TABLE
-
-if [ $? -ne 0 ]; then
-    echo "Error running GATK VariantsToTable"
-    exit 1
-fi
-
-bcftools index filled_af.vcf.gz
-
 echo "Job completed successfully at $(date)"
-
-#table.txt contains SNP info with AF
-#ouput filled_af.vcf.gz is the vcf 
-
